a569d540206eb3215665b20055a06c81f31af65d
mcozar99 <mcozartramblin@hawk.iit.edu>
2022-11-02 11:35:53 -0500

    bug fix

--- docker/task_definition_DataGatherer.sh
+++ docker/task_definition_DataGatherer.sh
@@ -7,7 +7,7 @@ set -m
 python3 main.py --action stream --frequency 1 --filename test.csv &
 
 # Start the helper process
-./../root/run_apache.sh
+sh /root/run_apache.sh
 
 # the my_helper_process might need to know how to wait on the
 # primary process to start before it does its work and returns
