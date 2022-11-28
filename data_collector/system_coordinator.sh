ab73ae54f1ac7923618efc031940342455ccfbcc
mcozar99 <mcozartramblin@hawk.iit.edu>
2022-11-27 19:25:51 -0600

    coordinator update

--- data_collector/system_coordinator.sh
+++ data_collector/system_coordinator.sh
@@ -1,14 +1,3 @@
-### Define all the commands to configure our EC2 instance and put it to work
-sudo yum update -y
-sudo amazon-linux-extras install docker
-sudo service docker start
-sudo systemctl enable docker
-sudo usermod -a -G docker ec2-user
-sudo yum install git
-
-# Repo downloading
-git clone https://github.com/mcozar99/trading_platform
-
 # Stream collector implementation
 mkdir streamcollector
 mv trading_platform/docker/DataGatherer streamcollector
